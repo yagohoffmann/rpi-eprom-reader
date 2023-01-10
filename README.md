523d22f97690e1afead4627a16f5dff9155fe9a4
Yago Hoffmann de Araujo <yago.araujo@datacom.ind.br>
2023-01-10 18:21:24 -0300

    Refine CMake for buildind gpiod lib
    Add gpiod headers and refine CMake for building and linking
    with the lib.
    
    Add cmake code for basic compiling and some information about
    cross compiling on readme.md
    
    Signed-off-by: Yago Hoffmann de Araujo <yago.araujo@datacom.ind.br>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rpi-eprom-reader
 Generic EPROM reader with raspberry pi
+
+
+For cross compile with CMake export CXX enviroment variable with g++ path.
+Ex: export CXX=/path/to/compiler/cross-pi-gcc-10.3.0-0/bin/arm-linux-gnueabihf-g++
+
+Cross compiler downloaded from https://sourceforge.net/projects/raspberry-pi-cross-compilers/files/
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 
+#include <gpiod.h>
+#include <error.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+struct gpiod_chip *chip;
+struct gpiod_line_request_config config;
+struct gpiod_line_bulk lines;
+
 int main(){
     std::cout << "Hello World!" << std::endl;
+
+    unsigned int offsets[1];
+
+    int values[1];
+    int err;
+
+    chip = gpiod_chip_open("/dev/gpiochip0");
+    if(!chip)
+    {
+      perror("gpiod_chip_open");
+    }
+
+    // set pin 21 to 1 (logic high)
+    offsets[0] = 14;
+    values[0] = 0;
+
+    err = gpiod_chip_get_lines(chip, offsets, 1, &lines);
+    if(err)
+    {
+      perror("gpiod_chip_get_lines");
+      return -1;
+    }
+
+    memset(&config, 0, sizeof(config));
+    config.consumer = "blink";
+    config.request_type = GPIOD_LINE_REQUEST_DIRECTION_OUTPUT;
+    config.flags = 0;
+
+    // get the bulk lines setting default value to 0
+    err = gpiod_line_request_bulk(&lines, &config, values);
+    if(err)
+    {
+      perror("gpiod_line_request_bulk");
+      return -1;
+    }
+
+    // output value 1 to turn on the led
+    values[0] = 1;
+    err = gpiod_line_set_value_bulk(&lines, values);
+    if(err)
+    {
+      perror("gpiod_line_set_value_bulk");
+      return -1;
+    }
+
+    sleep(1);
+
+    // output value 0 to turn off the led
+    values[0] = 0;
+    err = gpiod_line_set_value_bulk(&lines, values);
+    if(err)
+    {
+      perror("gpiod_line_set_value_bulk");
+      return -1;
+    }
+
+    gpiod_line_release_bulk(&lines);
+    gpiod_chip_close(chip);
+
     return 0;
 }
